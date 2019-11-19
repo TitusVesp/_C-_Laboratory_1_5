f22e2fa828ea4afbb1a6bfd8bd5941915cb87869
Kiril Riabov <kiril2906@hmail.com>
2019-11-19 09:18:19 +0200

    Add Function!

--- _C++_Laboratory_1_5/Laba5.cpp
+++ _C++_Laboratory_1_5/Laba5.cpp
@@ -9,14 +9,15 @@ public:
 	void Push(int obj) { data[position++] = obj; }
 	int Pop() { return data[--position]; }
 };
+
 void RNP()
 {
-	cout << "Imput your expression(with space!!!!): " << endl;
+	cout << "Imput your expression, if you want to imput func press F(with space!!!!): " << endl;
 	char* expr = new char[256];
 	cin.getline(expr, 256);
 
 	for (int i = 0; expr[i] > 0; i++) 
-		if (!isdigit(expr[i]) && expr[i] != '+' && expr[i] != '-' && expr[i] != '/' && expr[i] != '*' && expr[i] != '(' && expr[i] != ')' && expr[i] != ' ')
+		if (!isdigit(expr[i]) && expr[i] != '+' && expr[i] != '-' && expr[i] != '/' && expr[i] != '*' && expr[i] != '(' && expr[i] != ')' && expr[i] != ' ' && expr[i] != 'F')
 	{
 		cout << "Incorrect Input!" << endl;
 		return;
@@ -58,6 +59,9 @@ void RNP()
 				else
 					cout << "Error -> devision by zero!" << endl;
 				break;
+			case 'F':
+				st->Push(Func());
+				break;
 			case 'q':
 				cout << "Result: " << st->Pop() << endl;
 				break;
@@ -80,3 +84,75 @@ int Strlength(char* str)
 	return i;
 }
 
+int Func()
+{
+	cout << "Imput your expression: " << endl;
+	char* expr = new char[256];
+	char* expr_temp = new char[256];
+	cin.getline(expr, 256);
+	for (int i = 0, g = 0; i < Strlength(expr); i++)
+	{
+		if (expr[i] != ' ')
+			expr_temp[g++] = expr[i];
+	}
+	expr = expr_temp;
+	for (int i = 0; expr[i] > 0; i++) if (!isdigit(expr[i]) && expr[i] != '+' && expr[i] != '-' && expr[i] != '/' && expr[i] != '*' && expr[i] != '(' && expr[i] != ')')
+	{
+		cout << "Incorrect Input!" << endl;
+		return 0;
+	}
+	Areph expression(expr);
+	int result = expression.Expression();
+	return result;
+}
+
+int Areph::Number()
+{
+	int result = get() - '0';
+	while (peek() >= '0' && peek() <= '9')
+	{
+		result = 10 * result + get() - '0';
+	}
+	return result;
+}
+
+int Areph::Factor()
+{
+	if (peek() >= '0' && peek() <= '9')
+		return Number();
+	else if (peek() == '(')
+	{
+		get(); // '('
+		int result = Expression();
+		get(); // ')'
+		return result;
+	}
+	else if (peek() == '-')
+	{
+		get();
+		return -Factor();
+	}
+	return 0; // error
+}
+
+int Areph::Term()
+{
+	int result = Factor();
+	while (peek() == '*' || peek() == '/')
+		if (get() == '*')
+			result *= Factor();
+		else
+			result /= Factor();
+	return result;
+}
+
+int Areph::Expression()
+{
+	int result = Term();
+	while (peek() == '+' || peek() == '-')
+		if (get() == '+')
+			result += Term();
+		else
+			result -= Term();
+	return result;
+}
--- _C++_Laboratory_1_5/Laba5.h
+++ _C++_Laboratory_1_5/Laba5.h
@@ -4,6 +4,34 @@
 
 using namespace std;
 
+class Areph
+{
+private:
+	char* expressionToParse;
+	char peek()
+	{
+		return *expressionToParse;
+	}
+
+	char get()
+	{
+		return *expressionToParse++;
+	}
+
+	int Number();
+	int Factor();
+	int Term();
+public:
+	Areph(char* str)
+	{
+		expressionToParse = str;
+	}
+
+	int Expression();
+};
+
+
 void RNP();
 int Strlength(char* str);
 
+int Func();
